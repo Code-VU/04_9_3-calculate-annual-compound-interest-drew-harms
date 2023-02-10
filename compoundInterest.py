226174d4b3c8d5bb4ea21938ad440ed1a30a6b4f
Drew Harms <Drew.Harms@vu.com>
2023-02-10 13:15:21 -0600

    finished

--- compoundInterest.py
+++ compoundInterest.py
@@ -1,13 +1,19 @@
 def calculateCompoundInterest():
-    
-# This first 3 lines are provided for yougetACompoundInterest()
-# This first 3 lines are provided for you
- client_one_principal = float(input("Principle (amount): "))
- client_one_time =      float(input("Time:               "))
- client_one_rate =      float(input("Rate:               "))
- #print("Compound Interest: "+str(intrest))
+    interestFormula()
+    print("---")
+    interestFormula()
+    print("---")
+    interestFormula()
 
-    # end assignment
+
+def interestFormula():
+    client_principle = float(input("Principle (amount): "))
+    client_time = float(input("Time:               "))
+    client_rate = float(input("Rate:               "))
+    total_amount = client_principle * ((1 + (client_rate / 100)) ** client_time)
+    compInterest = (total_amount - client_principle)
+    print("Compound Interest: "+ str(round(compInterest,2)))
+ 
 
 ## If you want to test locally run > python compoundInterest.py
 
